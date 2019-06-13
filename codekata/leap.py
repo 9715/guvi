273fd961bdeeadca6e8bee3010a0268846109a9f
9715 <51650507+9715@users.noreply.github.com>
2019-06-13 12:34:52 +0530

    Update leap.py

--- codekata/leap.py
+++ codekata/leap.py
@@ -1,9 +1,9 @@
 y=int(input())
 if y%4==0 and y%100!=0:
-  print("Leap year")
+  print("yes")
 elif y%100==0:
-  print("It is not a leap year")
+  print("no")
 elif y%400==0:
-  print("It is a leap year")
+  print("yes")
 else:
-  print("It is not a leap year")
+  print("no")
